723ab66fa4925fe86ff91e567a2b98ff5416e695
l234567899638527 <WOFsoftware51@gmail.com>
2024-03-26 16:40:00 -0400

    4 new Autons

---
+++ src/main/java/frc/robot/autos/Red_Autos/Top_Autos/Red_Auto_Bumper_0_5.java
@@ -0,0 +1,53 @@
+package frc.robot.autos.Red_Autos.Top_Autos;
+
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.Transfer_IntakeCommand;
+import frc.robot.commands.Transfer_IntakeShoot;
+import frc.robot.commands_Auton.Auton_Wait;
+import frc.robot.subsystems.Auton_Subsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Transfer_Intake;
+import frc.robot.subsystems.Turret;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class Red_Auto_Bumper_0_5 extends SequentialCommandGroup {
+
+    public Red_Auto_Bumper_0_5(Swerve swerve, Turret turret, Shooter shooter, Auton_Subsystem aSub, Transfer_Intake transfer, Intake intake){
+        
+        addRequirements(swerve, turret, shooter, aSub, transfer, intake);
+
+        
+        addCommands(
+            new InstantCommand(() -> swerve.zeroGyro()),
+            new ParallelRaceGroup(
+                new Auton_Wait(100),
+                aSub.auton_Shooter_Start(shooter)
+            ),
+            new ParallelRaceGroup(
+                new Auton_Wait(100),
+                aSub.auton_Shoot(transfer)
+            ),
+            new ParallelRaceGroup(
+                new Transfer_IntakeCommand(transfer),
+                new IntakeCommand(intake),
+                new PathPlannerAuto("Red_Top_Bumper_0_5")
+            ),
+            new PathPlannerAuto("Red_Top_Bumper_5_0"),
+            new ParallelRaceGroup(
+                new Auton_Wait(100),
+                aSub.auton_Shoot(transfer)
+            ),
+            // new InstantCommand(() -> swerve.setYawWrapped(119.74)),
+            aSub.auton_Stop_Shooter(shooter)
+    
+        );   
+    }
+}
